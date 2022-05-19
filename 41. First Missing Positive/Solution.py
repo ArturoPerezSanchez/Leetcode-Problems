07b98362c50a96f1ab43e4ea14717c2f2ed97e13
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2022-05-19 19:46:15 +0200

    [new]: added solution to problem 41 with python

---
+++ 41. First Missing Positive/Solution.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/first-missing-positive
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        i= 0
+        while i < len(nums):
+            j = nums[i]-1
+            if (nums[i] > 0 and nums[i]<len(nums) and nums[i]!=nums[j]):
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i+=1 
+        for i in range(len(nums)):
+            if(i+1!= nums[i]):
+                return i+1
+        return len(nums)+1  
+                    
