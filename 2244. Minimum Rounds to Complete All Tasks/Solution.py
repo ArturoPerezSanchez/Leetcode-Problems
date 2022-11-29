5c339ff0f25e2f803e993f4eb52dd0130c8553af
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2022-11-29 18:52:30 +0100

    [new]: added solution to problem 2244 with python

---
+++ 2244. Minimum Rounds to Complete All Tasks/Solution.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks
+
+class Solution:
+    def minimumRounds(self, tasks: List[int]) -> int:
+        freqs = Counter(tasks)
+        res = 0
+        for task, freq in freqs.items():
+            if freq == 1:
+                return -1
+            res += (freq + 2) // 3
+        return res
