e576382dcf48ebf30df63a15168e5019439974e9
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-01-28 15:47:32 +0100

    [update]: Added aditional solution in C++

---
+++ 20. Valid Parentheses/Solution.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/valid-parentheses
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else if (st.empty() || (
+                      (c == ')' && st.top() != '(') ||
+                      (c == '}' && st.top() != '{') ||
+                      (c == ']' && st.top() != '[')
+                      )) {
+                return false;
+            } else {
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
