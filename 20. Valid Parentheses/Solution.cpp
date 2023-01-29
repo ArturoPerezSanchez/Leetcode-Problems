c9346c5e513b2df3f6f52765e7e5b225b5c189ca
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-01-29 17:47:32 +0100

    [refactor]: Added comments and complexity analysis

--- 20. Valid Parentheses/Solution.cpp
+++ 20. Valid Parentheses/Solution.cpp
@@ -2,23 +2,40 @@
 
 class Solution {
 public:
+    /*
+    Time complexity: O(n), where n is the length of the input string s. 
+    We iterate over each character in the string once, and each stack operation takes O(1) time.
+    
+    Space complexity: O(n), where n is the length of the input string s.
+    In the worst case, we may need to store all opening brackets on the stack.
+    */
     bool isValid(string s) {
-        stack<char> st;
+        // create an empty stack to store opening brackets
+        stack<char> st; 
 
-        for (char c : s) {
+        // iterate over each character in the input string
+        for (char c : s) { 
+
+            // push the character onto the stack if is an opening bracket
             if (c == '(' || c == '{' || c == '[') {
                 st.push(c);
-            } else if (st.empty() || (
+            
+            // if its a closing bracket, check whether the top of the stack contains the corresponding opening bracket
+            } else if (st.empty() || ( 
                       (c == ')' && st.top() != '(') ||
                       (c == '}' && st.top() != '{') ||
                       (c == ']' && st.top() != '[')
                       )) {
-                return false;
+
+                // if there is no matching opening bracket or the brackets do not match, return false
+                return false; 
             } else {
-                st.pop();
+                // otherwise, remove the top element from the stack
+                st.pop(); 
             }
         }
-
-        return st.empty();
+        // if the stack is empty, then all opening brackets have been matched and the string is valid
+        return st.empty(); 
     }
+
 };
