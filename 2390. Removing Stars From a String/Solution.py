00e9188d0e8944150fffb58e28fd01645874ede4
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-02-03 09:12:12 +0100

    [refactor]: added complexity analysis to problem 2390

--- 2390. Removing Stars From a String/Solution.py
+++ 2390. Removing Stars From a String/Solution.py
@@ -2,7 +2,16 @@
 
 class Solution:
 
-    #Using a stack and popping last added character when a star is found
+    '''
+    Using a stack and popping last added character when a star is found
+    Time Complexity: The function iterates over each character in the input string s exactly once, performing a constant amount of work for each character.
+                     The append and pop operations on the stack both take constant time on average. Therefore, the time complexity of this function is O(n),
+                     where n is the length of the input string.
+
+    Space Complexity: The function uses a stack data structure to store characters as it iterates over the input string.
+                      In the worst case, where there are no stars in the input string, the stack will contain all the characters in the input string.
+                      Therefore, the space complexity of this function is also O(n), where n is the length of the input string.
+    '''
     def removeStars(self, s: str) -> str:
         stack = []  # create an empty stack
 
