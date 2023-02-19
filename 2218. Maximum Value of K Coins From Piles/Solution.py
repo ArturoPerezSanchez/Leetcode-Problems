b7779fc2c8401cda363312b7312ba057a02da648
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-02-19 15:47:32 +0100

    [new]: added solution to problem 1431 with python

---
+++ 2218. Maximum Value of K Coins From Piles/Solution.py
@@ -0,0 +1,63 @@
+# https://leetcode.com/problems/maximum-value-of-k-coins-from-piles/
+
+class Solution:
+    def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
+        
+        # Get the number of piles and increment k
+        n = len(piles)
+        k += 1
+        
+        # Initialize previous array and the counter
+        prev = [0]
+        count = 0
+        
+        # Loop through each pile
+        for i in range(n):
+            
+            # Get the current pile
+            pile = piles[i]
+            acum = 0
+            
+            # Loop through each coin in the pile
+            for j in range(len(pile)):
+                
+                # Get the value of the current coin and update the pile
+                val = pile[j]
+                pile[j] = acum
+                acum +=val
+                
+                # Increment the counter
+                count +=1
+            
+            # Add the accumulated value of the pile to the end of the pile
+            pile.append(acum)
+            # Add the current count to the previous array
+            prev.append(count)
+
+        # Initialize the dp array
+        dp = [[0]*k for i in range(count+1)]
+        
+        # Loop through each pile
+        for i in range(n):
+            
+            # Loop through each coin in the pile
+            for j in range(1, len(piles[i])):
+                
+                # Get the profit and weight of the current coin
+                profit = piles[i][j]
+                wt = j
+                
+                # Get the level of the current coin
+                lvl = prev[i]+j
+                
+                # Loop through each weight
+                for w in range(1, k):
+                    
+                    # Update the dp array based on the weight and profit
+                    if wt > w:
+                        dp[lvl][w] = dp[lvl-1][w]
+                    else:
+                        dp[lvl][w] = max(dp[lvl-1][w], profit+dp[prev[i]][w-wt])
+
+        # Return the last element of the last row of the dp array
+        return dp[-1][-1]
