4f5c898f9af348de9092750d7f024a56bda79903
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-03-19 12:56:12 +0100

    [Update]: Added time complexity analysis

--- 879. Profitable Schemes/Solution.py
+++ 879. Profitable Schemes/Solution.py
@@ -2,33 +2,38 @@
 
 
 class Solution:
-    
+    '''
+	Time complexity: O(2^n), where n is length of the group list, since the recursive function performs contant operations and its called 2 times per level. Aditionally,
+					 the creation of the list of tuples l takes O(nlogn) time to create since the list lenght is n and the sorting algorithm takes (nlogn) to sort the whole list. 
+
+	Space complexity: O(n), where n is length of the group list, since the recursion tree can have at most n levels. Aditionally we also create a list of tuples of n elements.
+    '''
     def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
         
         # Create a list of tuples (group, profit) sorted by group
         l = sorted(zip(group, profit))
 
         # Define a recursive function
-        #   remaining_members: int representing the members that havent commited a crime yet
-        #   currentProfit: int representing the accumulated profit (if its bigger than minProfit, we simply pass minProfit as parameter)
-        #   ind: int index that iterates through the l previously created list of tuples
-        # the cache decorator its used to avoid the execution of the function with the same parameters more than once
+        #   remaining_members: int representing the members that havent commited a crime yet.
+        #   currentProfit: int representing the accumulated profit (if its bigger than minProfit, we simply pass minProfit as parameter).
+        #   ind: int index that iterates through the l previously created list of tuples.
+        # the cache decorator its used to avoid the execution of the function with the same parameters more than once.
         @cache
         def rec (remaining_members, currentProfit, ind):
             
             # Base case: If we have iterated through the whole list or the group members required for current element of the list is bigger than the remaining members.
-            # 			 Return 1 if accumulated profit is greater or equal than minProfit, otherwise return 0
+            # 			 Return 1 if accumulated profit is greater or equal than minProfit, otherwise return 0.
             if (ind>=len(l) or l[ind][0] > remaining_members):
                 return 1 if (currentProfit >= minProfit) else 0
             
 
             # Recursive Case: Call the function selecting the ind and not selecting it, for better performance, when selecting the current crime
-            # 				  check if we have reached the required minProfit, if so simply set it as the currentProfit parameter
+            # 				  check if we have reached the required minProfit, if so simply set it as the currentProfit parameter.
             r1 = rec(remaining_members - l[ind][0], min(currentProfit + l[ind][1], minProfit), ind+1)
             r2 = rec(remaining_members, currentProfit, ind+1)
 
-            # add the result of the 2 recursive calls and return it
+            # add the result of the 2 recursive calls and return it.
             return r1 + r2
         
-        # Call the recursive function rec() with initial parameters n, 0, and 0 and return the result modulo 10^9+7
+        # Call the recursive function rec() with initial parameters n, 0, and 0 and return the result modulo 10^9+7.
         return rec(n, 0, 0)%(10**9+7)
