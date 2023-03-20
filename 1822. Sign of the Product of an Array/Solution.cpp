a8cc271c78b994719dc3cfab159e21a39ed719a4
ArturoPerezSanchez <arturoperez.bs@gmail.com>
2023-03-20 15:47:32 +0100

    [update]: added solution to problem 1822 with C++

---
+++ 1822. Sign of the Product of an Array/Solution.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/sign-of-the-product-of-an-array/description/
+
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        // Initialize a variable to keep track of the sign of the product of all elements in the vector
+        int res = 1;
+
+        // Iterate through each element in the vector
+        for (int i: nums) {
+            // If an element is 0, return 0 as the product of all elements will also be 0
+            if (i == 0) {
+                return 0;
+            }
+            // If an element is negative, flip the sign of the res variable
+            if (i < 0) {
+                res *= -1;
+            }
+        }
+        // If the number of negative numbers is even, res will remain 1
+        // Otherwise, if the number of negative numbers is odd, res will be -1
+        return res;
+    }
+};
